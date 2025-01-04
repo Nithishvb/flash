a13f0d8707e32153d8abda68cee4a71a2e6ba8a4
Nithishvb <Nithishvb@users.noreply.github.com>
2025-01-04 13:13:02 +0530

    feat: Serve asset files from the development server

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -7,12 +7,43 @@ import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
 import generate from "@babel/generator";
 import * as t from "@babel/types";
+import mime from "mime-types";
 
 const hostname = "localhost";
 const port = 5000;
 const TARGET_DIR = path.join(process.cwd(), "flash-demo-app");
 
 const program = new Command();
+
+const imageExtensions = [
+  'jpg',
+  'jpeg',
+  'png',
+  'gif',
+  'bmp',
+  'tiff',
+  'svg',
+  'webp',
+  'heif',
+  'heic',
+  'avif',
+  'eps',
+  'pdf',
+  'ai',
+  'raw',
+  'cr2',
+  'nef',
+  'orf',
+  'sr2',
+  'apng', // Animated PNG
+  'ico',  // Icon format
+  'xbm',  // X BitMap
+  'pbm',  // Portable Bitmap
+  'pgm',  // Portable Graymap
+  'ppm',  // Portable Pixmap
+  'exr',  // OpenEXR
+];
+
 
 program
   .name("flash")
@@ -23,72 +54,111 @@ program
     const server = http.createServer(async (req, res) => {
       console.log(`Request received: ${req.method} ${req.url}`);
 
-      if (req.url === "/favicon.ico") {
-        res.writeHead(204);
-        res.end();
-        return;
-      }
+      if (req.url) {
+        if (req.url === "/favicon.ico") {
+          res.writeHead(204);
+          res.end();
+          return;
+        }
 
-      const initialHtmlTemplate = path.join(TARGET_DIR, "index.html");
+        const initialHtmlTemplate = path.join(TARGET_DIR, "index.html");
+        const filePath = path.join(TARGET_DIR, req?.url);
 
-      if (req.url === "/") {
-        fs.readFile(
-          initialHtmlTemplate,
-          "utf8",
-          (err: NodeJS.ErrnoException | null, data: string) => {
-            if (err) {
-              console.error("Error reading file:", err);
-              res.writeHead(500, { "Content-Type": "text/plain" });
-              res.end("Internal Server Error");
-              return;
-            }
+        const query = req.url.split("?")[1];
 
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);
-          }
-        );
-        return;
-      } else {
-        if (req.url) {
-          const filePath = path.join(TARGET_DIR, req.url);
-          if (req.url.includes(".css")) {
-            if (fs.existsSync(filePath)) {
-              const cssContent = fs.readFileSync(filePath, "utf-8");
-              const jsResponse = `
-                const __cssContent = ${JSON.stringify(cssContent)};
-                const __cssId = ${JSON.stringify(filePath)};
-                (function() {
-                  let style = document.querySelector(\`style[data-id="\${__cssId}"]\`);
-                  if (!style) {
-                    style = document.createElement("style");
-                    style.setAttribute("data-id", __cssId);
-                    document.head.appendChild(style);
-                  }
-                  style.textContent = __cssContent;
-                })();
-              `;
-              res.writeHead(200, { "Content-Type": "application/javascript" });
-              res.end(jsResponse);
-              return;
-            }
+        // Handle requests that contain the "import" query parameter
+        if (query === "import") {
+          const filePath = path.join(TARGET_DIR, req.url.split("?")[0]);
+      
+          if (fs.existsSync(filePath)) {
+            const fileUrl = `/${path.relative(TARGET_DIR, filePath).replace(/\\/g, "/")}`;
+            const jsResponse = `export default ${JSON.stringify(fileUrl)};`;
+            res.writeHead(200, { "Content-Type": "application/javascript" });
+            res.end(jsResponse);
+            return;
           } else {
-            // Serve directly if the request is for node_modules
-            if (req.url.includes("node_modules")) {
-              if (fs.existsSync(filePath)) {
-                const data = fs.readFileSync(filePath);
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Asset not found");
+            return;
+          }
+        }
 
-                res.writeHead(200, {
-                  "Content-Type": "application/javascript",
-                });
-                res.end(data);
-              } else {
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end("File not found");
+        if (req.url === "/") {
+          fs.readFile(
+            initialHtmlTemplate,
+            "utf8",
+            (err: NodeJS.ErrnoException | null, data: string) => {
+              if (err) {
+                console.error("Error reading file:", err);
+                res.writeHead(500, { "Content-Type": "text/plain" });
+                res.end("Internal Server Error");
+                return;
               }
-              return;
-            }
 
-            // Process user source files with esbuild
+              res.writeHead(200, { "Content-Type": "text/html" });
+              res.end(data);
+            }
+          );
+          return;
+        }
+
+        // Handle CSS files
+        if (req.url.includes(".css")) {
+          if (fs.existsSync(filePath)) {
+            const cssContent = fs.readFileSync(filePath, "utf-8");
+            const jsResponse = `
+            const __cssContent = ${JSON.stringify(cssContent)};
+            const __cssId = ${JSON.stringify(filePath)};
+            (function() {
+              let style = document.querySelector(\`style[data-id="\${__cssId}"]\`);
+              if (!style) {
+                style = document.createElement("style");
+                style.setAttribute("data-id", __cssId);
+                document.head.appendChild(style);
+              }
+              style.textContent = __cssContent;
+            })();
+          `;
+            res.writeHead(200, { "Content-Type": "application/javascript" });
+            res.end(jsResponse);
+            return;
+          }
+        }
+
+        //Handle image files
+        const mimeType = mime.lookup(filePath);
+        if (mimeType && mimeType.startsWith("image/")) {
+          const mimeType = mime.lookup(filePath); // Get the MIME type based on file extension
+          if (mimeType && mimeType.startsWith("image/")) {
+            const imageFile = fs.readFileSync(filePath); // Read the binary data of the image
+            res.writeHead(200, { "Content-Type": mimeType }); // Set the correct Content-Type
+            res.end(imageFile); // Send the binary image data
+          }
+        }
+
+        //Handle dependency files
+        if (req.url && req.url.includes("node_modules")) {
+          if (fs.existsSync(filePath)) {
+            const data = fs.readFileSync(filePath);
+
+            res.writeHead(200, {
+              "Content-Type": "application/javascript",
+            });
+            res.end(data);
+          } else {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("File not found");
+          }
+          return;
+        }
+
+        // Handle JavaScript or TypeScript files
+        if (
+          filePath.endsWith(".js") ||
+          filePath.endsWith(".ts") ||
+          filePath.endsWith(".tsx")
+        ) {
+          try {
             const result = await build({
               entryPoints: [filePath],
               bundle: false,
@@ -102,16 +172,20 @@ program
               jsx: "automatic",
             });
 
-            // Rewrite bare imports for user source files
             const modifiedCode = await rewriteBareImports(
               result.outputFiles[0].text
             );
-
             res.writeHead(200, { "Content-Type": "application/javascript" });
             res.end(modifiedCode);
+          } catch (error) {
+            console.error("Error processing JS/TS file:", error);
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal Server Error");
           }
+          return;
         }
       }
+
     });
 
     server.listen(port, hostname, async () => {
@@ -123,38 +197,7 @@ program
 export default program;
 
 async function rewriteBareImports(code: string) {
-  // if (code) {
-  //   const updatedCode = code.replace(
-  //     /from\s+["']([^./][^"']*)["']/g,
-  //     (match, packageName) => {
-  //       console.log("Matched import", match);
-  //       let resolvedPath = path.join(TARGET_DIR, "node_modules", packageName);
 
-  //       if (packageName === "react") {
-  //         return `from "/node_modules/react/cjs/react.development.js"`;
-  //       }
-  //       if (packageName === "react-dom") {
-  //         return `from "/node_modules/react-dom/cjs/react-dom.development.js"`;
-  //       }
-
-  //       if (!fs.existsSync(resolvedPath)) {
-  //         resolvedPath = `${resolvedPath}.js`;
-  //       }
-
-  //       const stats = fs.statSync(resolvedPath);
-
-  //       if (fs.existsSync(resolvedPath)) {
-  //         if (stats.isDirectory()) {
-  //           return `from "/node_modules/${packageName}/index.js"`;
-  //         }
-  //         return `from "/node_modules/${packageName}.js"`;
-  //       }
-  //       console.log("operatijm ended");
-  //       throw new Error(`Package ${packageName} not found`);
-  //     }
-  //   );
-  //   return updatedCode;
-  // }
   const ast = parse(code, {
     sourceType: "module", // Treat as an ES module
     plugins: ["jsx"], // Add support for JSX if needed
@@ -163,6 +206,9 @@ async function rewriteBareImports(code: string) {
   traverse(ast, {
     ImportDeclaration(path) {
       const importPath = path.node.source.value;
+
+      const splitImportPath = importPath.split(".");
+
       if (importPath === "react-dom/client") {
         // Generate a new unique import variable
         const importVariableName = "__flash_import_reactDom_client";
@@ -198,16 +244,14 @@ async function rewriteBareImports(code: string) {
         path.node.source.value = preBundledPath;
 
         let indentifierName: string = "";
-        
-        path.node.specifiers.forEach((specifier) => { 
+
+        path.node.specifiers.forEach((specifier) => {
           if (t.isImportSpecifier(specifier)) {
             const importedName = specifier.imported;
             const localName = specifier.local.name;
             indentifierName = specifier.local.name;
-            console.log("imported name" ,importedName);
-            console.log("imported name", localName);
           }
-        })
+        });
 
         // Rewrite the import to a default import
         path.node.specifiers = [
@@ -281,6 +325,31 @@ async function rewriteBareImports(code: string) {
         path.insertAfter(createJsxDeclaration);
         path.insertAfter(createJsxsDeclaration);
       }
+
+      //Insert import for the assets import statements
+      if(splitImportPath.length > 0){
+        const result = imageExtensions.includes(splitImportPath[splitImportPath.length - 1]);
+        if(result){
+          path.node.source.value = `${importPath}?import`;
+        }
+      }
+
+      //non react modules imports
+      // const nonReactModuleFilePath = `/node_modules/.flash/deps/${importPath},js`;
+      // if(fs.readFileSync(nonReactModuleFilePath)){
+      //   path.node.source.value = nonReactModuleFilePath;
+      // }else{
+      //   const buildPath = `/node_modules/${importPath}`;
+      //   const result = await build({
+      //     entryPoints: [buildPath],
+      //     outfile: path.join(`/node_modules/.flash/deps`, `${importPath}.js`),
+      //     bundle: true,
+      //     format: "esm",
+      //     platform: "browser",
+      //     metafile: true,
+      //   });
+
+      // }
     },
   });
 
